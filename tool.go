6a7b7516a93ab2d4ad82b667f41f2748086a8f88
QGitter <1007468909@qq.com>
2019-06-13 17:50:08 +0800

    Update tool.go

--- tool.go
+++ tool.go
@@ -68,6 +68,22 @@ func convertToBin(n int) string {
 	return result
 }
 
+//计算字符串中最大不重复的字串的长度
+func substrNoRepeat(s string) int {
+	lastOccurred := make(map[byte]int)
+	start := 0
+	maxLength := 0
+	for i, ch := range []byte(s) {
+		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
+			start = lastI + 1
+		}
+		if i-start+1 > maxLength {
+			maxLength = i - start + 1
+		}
+		lastOccurred[ch] = i
+	}
+	return maxLength
+}
 
 
 
